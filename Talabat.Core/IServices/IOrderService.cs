564b7db989ce190de8d27418ae8d196a2005b1ff
Hamza <hamza_31056248@f-eng.tanta.edu.eg>
2025-02-28 16:14:04 +0200

    Adding IOredrService and orderService

---
+++ Talabat.Core/IServices/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Talabat.Core.Entities.Order_Aggregate;
+
+namespace Talabat.Core.IServices
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string customerEmail, string BasketId, int deleveryMethodId, Address orderAddress);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string userEmail);
+        Task<Order> GetOrderForUserAsync(int OrderId , string userEmail);
+    }
+}
---
+++ Talabat.Service/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.IServices;
+
+namespace Talabat.Service
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string customerEmail, string BasketId, int deleveryMethodId, Address orderAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderForUserAsync(int OrderId, string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
