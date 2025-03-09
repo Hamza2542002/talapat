770d3ac060704e831668c678cc987f26f228e349
Hamza <hamza_31056248@f-eng.tanta.edu.eg>
2025-03-09 13:06:26 +0200

    Adding Product Service

---
+++ Talabat.Core/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Talabat.Core.Entities;
+using Talabat.Core.Specifications.ProductSpecs;
+
+namespace Talabat.Core.IServices
+{
+    public interface IProductService
+    {
+        Task<IReadOnlyList<Product>> GetProductsAsync(ProductSpecsParams productParamsModel);
+        Task<int> GetCountAsync(ProductSpecsParams productParamsModel); 
+        Task<Product?> GetProductAsync(int id);
+        Task<IReadOnlyList<Brand>> GetBrandsAsync();
+        Task<IReadOnlyList<Category>> GetCategoriesAsync();
+    }
+}
---
+++ Talabat.Service/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.IServices;
+using Talabat.Core.Specifications.ProductSpecs;
+
+namespace Talabat.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(ProductSpecsParams productParamsModel)
+        {
+            var products =
+                await _unitOfWork.GetRepository<Product>().GetAllWithSpecsAsync(new ProductSpecifications(productParamsModel));
+
+            return products;
+        }
+        public async Task<int> GetCountAsync(ProductSpecsParams productParamsModel)
+        {
+            return await _unitOfWork
+                .GetRepository<Product>().GetCountAsync(new ProductCountSpecs(productParamsModel));
+        }
+        public async Task<Product?> GetProductAsync(int id)
+        {
+            var product = await _unitOfWork
+                .GetRepository<Product>().GetWithSpecsAsync(new ProductSpecifications(id));
+
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Brand>> GetBrandsAsync()
+        {
+            return await _unitOfWork
+                .GetRepository<Brand>().GetAllAsync();
+        }
+
+        public async Task<IReadOnlyList<Category>> GetCategoriesAsync()
+        {
+            return await _unitOfWork
+                .GetRepository<Category>().GetAllAsync();
+        }
+    }
+}
